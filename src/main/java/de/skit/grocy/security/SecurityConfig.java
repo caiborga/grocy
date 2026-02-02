103208003556b6cfe953534792f9468c480cdfa4
Stephan Kleinhans <4.da.crap@gmail.com>
2026-02-02 16:36:06 +0100

    backend accept join

--- src/main/java/de/skit/grocy/security/SecurityConfig.java
+++ src/main/java/de/skit/grocy/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/", "/index.html",
                                 "/assets/**", "/favicon.ico",
-                                "/*.js", "/*.css", "/*.svg", "/*.png", "/*.woff2")
+                                "/*.js", "/*.css", "/*.svg", "/*.png", "/*.woff2", "/join")
                         .permitAll()
 
                         // Public API
